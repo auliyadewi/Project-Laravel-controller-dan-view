41a5714cc45ea160039920435c6c865b784a2291
auliyadewi <139748451+auliyadewi@users.noreply.github.com>
2023-09-24 22:16:34 +0700

    first commit

---
+++ resources/views/Info_kampus.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Innfo Kampus</title>
+</head>
+<body>
+    <h1>Informasi Kampus</h1>
+    <p>Universitas Pembangunan Nasional Veteran Jakarta atau yang dikenal dengan UPN Veteran Jakarta ini memang baru sekitar 6 tahun beralih status menjadi Perguruan Tinggi Negeri. Namun, UPN Veteran Jakarta (UPNVJ) pada dasarnya sudah eksis sejak tahun 1977 lho! Dahulu, UPN Veteran Jakarta merupakan perguruan tinggi kedinasan yang operasionalnya berada di bawah Kementerian Pertahanan dan kemudian berafiliasi dengan yayasan terkait kementerian tersebut dan akhirnya menjadi sebuah PTN. Ada banyak banget keunggulan dari kampus Bela Negara ini yang bisa menjadi acuan kamu untuk melanjutkan pendidikan di bangku kuliah nanti.</p><br>
+    <h2>Informasi dan Sejarah Kampus Universitas Pembangunan Nasional veteran Jakarta Fakultas Ilmu Komputer</h2><br>
+    <p>Sejarah Fakultas Ilmu Komputer UPN “Veteran” Jakarta tidak dapat dipisahkan dari sejarah UPN “Veteran” Jakarta itu sendiri. Sebelum menjadi institusi pendidikan tinggi yang berdiri sendiri, UPN “Veteran” Jakarta memiliki rangkaian perjalanan sejarah yang cukup panjang yaitu berdasarkan Akte Notaris R. Kardiman Nomor: 14  tanggal 7 Januari 1963, Lembaga Pembinaan Kader Pembangunan (LPKP) mendirikan 3 (tiga) akademi, yaitu: 1) Akademi Bank; 2) Akademi Tekstil; dan 3) Akademi Tata Laksana Pelayaran Niaga “Yos Sudarso”.</p>
+    <p>Pada tahun 1967, ketiga akademi tersebut diintegrasikan kedalam PTPN Veteran dengan nama PTPN Veteran Cabang Jakarta melalui Surat Keputusan Menteri Urusan Veteran dan Demobilisasi RI Nomor : 09/Kpts/Menved/1967, tanggal 21 Pebruari 1967. Kemudian berdasarkan Surat Keputusan Menhankam/Pangab Nomor: Skep/1555/1977 tanggal 30 November 1977 PTPN “Veteran” Jakarta berubah nama menjadi Universitas Pembangunan Nasional (UPN) “Veteran” Cabang Jakarta.</p>
+    <p>Pada tahun 1980, UPN “Veteran” Cabang Jakarta menambah program pendidikannya, yaitu Pendidikan Ahli Teknik Informatika dan Komputer (PATIK) dengan jenjang pendidikan Diploma 3. Program Pendidikan Ahli Teknik Informatika dan Komputer UPN “Veteran” Cabang Jakarta merupakan salah satu program pendidikan yang banyak diminati oleh masyarakat, karena saat itu program pendidikan teknik informatika dan komputer masih sangat langka di wilayah Jakarta dan sekitarnya.</p>
+    <p>Seiring perjalanan waktu, pada tahun 1993, UPN “Veteran” Cabang Jakarta menjadi perguruan tinggi yang mandiri berdasarkan Keputusan Menhankam Nomor: Kep/03/II/1993 tanggal 27 Februari 1993 terdiri dari 3 (tiga) Fakultas yang menyelenggarakan Program Pendidikan S-1 dan D-III dengan status “Kedinasan”. Pada tanggal 1 Mei 1995, berdasarkan Surat Keputusan Rektor UPN “Veteran” Jakarta Nomor: Skep/031/V/1995 tentang Perubahan Fakultas Teknik menjadi tiga fakultas, yaitu: 1) Fakultas Teknologi Industri; 2) Fakultas Ilmu Komputer; dan 3) Fakultas Teknologi Kelautan yang diikuti dengan terbitnya Keputusan Mendikbud Nomor Kep./017.018.019/D/0/1995 tentang perubahan UPN “Veteran” Jakarta yang semula perguruan tinggi dengan status “Kedinasan” berubah menjadi perguruan tinggi swasta dengan status “DISAMAKAN” untuk seluruh jenjang pendidikan Diploma 3, dan status “TERDAFTAR” untuk seluruh jenjang pendidikan Strata 1.</p>
+    <p>Pada tanggal 6 Oktober 2014, berdasarkan Peraturan Presiden Nomor 120 tahun 2014 tentang Pendirian UPN “Veteran” Jakarta, maka UPN “Veteran” Jakarta berubah dari Perguruan Tinggi Swasta (PTS) menjadi Perguruan Tinggi Negeri (PTN) di bawah pembinaan Kementerian Riset, Teknologi, dan Pendidikan Tinggi.</p>
+
+    <p><a href="https://www.upnvj.ac.id/">Website UPNVJ</a><br></p>
+    <p><a href="https://new-fik.upnvj.ac.id/tentang-fik/">Website UPNVJ Fakultas Ilmu Komputer</a></p>
+    <a href="/halaman depan">Kembali Ke Halaman Depan</a><br>
+    
+</body>
+</html>
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Profile Pembuat</title>
+</head>
+<body>
+    <h1>Profile Pembuat Aplikasi</h1>
+    <form>
+        <P>Nama : Auliya Dewi Safitri</P><br>
+        <p>Tempat, tanggal lahir : 16, April 2003</p><br>
+        <p>Jenis Kelamin : Perempuan</p><br>
+        <p>Status : Mahasiswa</p><br>
+        <p>Jenis Kelamin : Perempuan</p><br>
+        <p>Jurusan : Sistem Informasi</p><br>
+        <p>Fakultas : Ilmu Komputer</p><br>
+        <p>Universitas : Pembangunan Nasional veteran Jakarta</p><br>
+        <p><a href="/kampus info">Info Kampus</a></p>
+        <p><a href="/halaman depan">Halaman Depan</a></p>
+    </form>
+</body>
+</html>
